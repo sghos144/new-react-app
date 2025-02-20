9e81297d9e87209908a5b528ef9d7a7e617858ca
sghos144 <suman_ghosh@optum.com>
2025-02-20 11:25:35 +0530

    Added event handling : MouseEvent

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,17 +1,20 @@
+import { MouseEvent } from "react";
+
 function ListGroup() {
-  const items: any[] = [];
-//   const items = ["New York", "London", "Tokyo", "San Francisco", "Paris"];
-  const list_condition = items.length === 0 ? <p>No item found</p> : null
+//   const items: any[] = [];
+  const items = ["New York", "London", "Tokyo", "San Francisco", "Paris"];
+  const list_condition = items.length === 0 && <p>No item found</p>
+  const handleClick = (event:MouseEvent) => console.log(event);
+
+
   return (
-    <>
+      <>
       <h1>List</h1>
       {list_condition}
       <ul className="list-group">
-        <li className="list-group-item active" aria-current="true">
-          {items[0]}
-        </li>
-        {items.slice(1).map((item, id) => (
-          <li className="list-group-item" key={id}>
+        {items.map((item, id) => (
+        
+          <li className="list-group-item" key={id} onClick={handleClick}>
             {item}
           </li>
         ))}
