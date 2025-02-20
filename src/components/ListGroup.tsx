5d13e94fac711973effbf041447fed96be69046b
sghos144 <suman_ghosh@optum.com>
2025-02-20 12:02:00 +0530

    Managing state : useState

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,20 +1,27 @@
-import { MouseEvent } from "react";
+import { useState } from "react";
 
 function ListGroup() {
-//   const items: any[] = [];
+  //   const items: any[] = [];
   const items = ["New York", "London", "Tokyo", "San Francisco", "Paris"];
-  const list_condition = items.length === 0 && <p>No item found</p>
-  const handleClick = (event:MouseEvent) => console.log(event);
-
+  const list_condition = items.length === 0 && <p>No item found</p>;
+  //   const handleClick = (event:MouseEvent) => console.log(event);
+  const [selectedIndex, setSelectedIndex] = useState(-1); // Managing State
 
   return (
-      <>
+    <>
       <h1>List</h1>
       {list_condition}
       <ul className="list-group">
-        {items.map((item, id) => (
-        
-          <li className="list-group-item" key={id} onClick={handleClick}>
+        {items.map((item, index) => (
+          <li
+            className={
+              selectedIndex === index
+                ? "list-group-item active"
+                : "list-group-item"
+            }
+            key={index}
+            onClick={() => {setSelectedIndex(index); console.log(index + ' : ' + item)}}
+          >
             {item}
           </li>
         ))}
